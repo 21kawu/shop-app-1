4a017f0844253713b0f5ce019d617a5a386ad27d
Kawu <rekkibass@gmail.com>
2026-02-01 22:24:19 +0100

    Dodanie całej struktury projektu (backend i frontend)

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/send", async (req, res) => {
+  const { items } = req.body;
+
+  try {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "rekkibass@gmail.com",
+        pass: "ndfmquyetmcefenx",
+      },
+    });
+
+    await transporter.sendMail({
+      from: "rekkibass@gmail.com",
+      to: "rekkibass@gmail.com",
+      subject: "Nowe zamówienie",
+      text: `Wybrane produkty: ${items}`,
+    });
+
+    res.json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Błąd wysyłania" });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Serwer działa na porcie 3001");
+});
